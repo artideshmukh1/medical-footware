4149ab61045857933b25ca78db95adb29e71936a
artideshmukh1 <60171742+artideshmukh1@users.noreply.github.com>
2024-02-27 14:23:01 +0530

    Delete src directory

--- src/main/java/com/project/medicalfootware/repositories/FeedbackRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.medicalfootware.repositories;
-
-import com.project.medicalfootware.models.Feedback;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
-}
